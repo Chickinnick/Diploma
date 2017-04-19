f50b9ddd212bfb742380883c366237eec7746ccf
chickinnick <chickinnick@gmail.com>
2017-04-19 19:45:36 +0300

    recycler impl

--- app/src/main/java/com/umbaba/bluetoothvswifidirect/MainActivity.java
+++ app/src/main/java/com/umbaba/bluetoothvswifidirect/MainActivity.java
@@ -1,29 +1,11 @@
 package com.umbaba.bluetoothvswifidirect;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 
-import com.github.ivbaranov.rxbluetooth.Action;
-import com.github.ivbaranov.rxbluetooth.RxBluetooth;
 import com.umbaba.bluetoothvswifidirect.comparation.ComparationFragment;
 import com.umbaba.bluetoothvswifidirect.comparation.ComparationPresenter;
 import com.umbaba.bluetoothvswifidirect.util.ActivityUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends FragmentActivity {
 
--- app/src/main/java/com/umbaba/bluetoothvswifidirect/comparation/ComparationFragment.java
+++ app/src/main/java/com/umbaba/bluetoothvswifidirect/comparation/ComparationFragment.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.umbaba.bluetoothvswifidirect.R;
 
@@ -41,7 +44,42 @@ public class ComparationFragment extends Fragment implements ComparationContract
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comparation, container, false);
+        View view = inflater.inflate(R.layout.fragment_comparation, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.comparation_recycler);
+        return view;
     }
 
+
+    public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ComparationViewHolder>{
+
+
+
+        @Override
+        public ComparationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ComparationViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class ComparationViewHolder extends RecyclerView.ViewHolder {
+            CardView cv;
+            TextView criterio;
+            TextView left;
+            TextView right;
+
+            ComparationViewHolder(View itemView) {
+                super(itemView);
+               // cv = (CardView) itemView.findViewById(R.id.cv);
+            }
+        }
+
+    }
 }
