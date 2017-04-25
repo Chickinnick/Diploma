5bd51a07232405636b3eb72e141ffdada5200f8f
chickinnick <chickinnick@gmail.com>
2017-04-25 19:58:38 +0300

    ble chenged ui

--- app/src/main/java/com/umbaba/bluetoothvswifidirect/bluetooth/BluetoothFragment.java
+++ app/src/main/java/com/umbaba/bluetoothvswifidirect/bluetooth/BluetoothFragment.java
@@ -3,34 +3,38 @@ package com.umbaba.bluetoothvswifidirect.bluetooth;
 
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.umbaba.bluetoothvswifidirect.R;
+import com.umbaba.bluetoothvswifidirect.comparation.ComparationFragment;
 
 import java.util.List;
 
 import static com.google.gson.internal.$Gson$Preconditions.checkNotNull;
+import static java.util.Collections.EMPTY_LIST;
 
 
-public class BluetoothFragment extends Fragment implements BluetoothContract.View{
+public class BluetoothFragment extends Fragment implements BluetoothContract.View {
 
     public static final int ID = 12;
-
+    private static final String TAG = "BluetoothFragment";
     private Button start;
     private Button stop;
-    private ListView result;
+    private RecyclerView recyclerView;
 
     private BluetoothContract.Presenter mPresenter;
+    private RVAdapter adapter;
 
     public BluetoothFragment() {
 
@@ -58,7 +62,7 @@ public class BluetoothFragment extends Fragment implements BluetoothContract.Vie
 
 
     @Override
-    public void setPresenter (@NonNull BluetoothContract.Presenter presenter) {
+    public void setPresenter(@NonNull BluetoothContract.Presenter presenter) {
         mPresenter = checkNotNull(presenter);
 
     }
@@ -71,8 +75,8 @@ public class BluetoothFragment extends Fragment implements BluetoothContract.Vie
         View inflate = inflater.inflate(R.layout.fragment_bluetooth, container, false);
         start = (Button) inflate.findViewById(R.id.start);
         stop = (Button) inflate.findViewById(R.id.stop);
-        result = (ListView) inflate.findViewById(R.id.result);
-
+        recyclerView = (RecyclerView) inflate.findViewById(R.id.result);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return inflate;
     }
 
@@ -83,14 +87,17 @@ public class BluetoothFragment extends Fragment implements BluetoothContract.Vie
 
 
         start.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 mPresenter.start();
-                setAdapter(mPresenter.getDevices());
 
+                adapter = new BluetoothFragment.RVAdapter(mPresenter.getDevices());
+                recyclerView.setAdapter(adapter);
             }
         });
         stop.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 mPresenter.stop();
             }
         });
@@ -124,13 +131,59 @@ public class BluetoothFragment extends Fragment implements BluetoothContract.Vie
         if (!TextUtils.isEmpty(device.getName())) {
             deviceName += " " + device.getName();
         }
-         setAdapter(mPresenter.getDevices());
-    }
-
-    private void setAdapter(List<String> list) {
-        int itemLayoutId = android.R.layout.simple_list_item_1;
-        result.setAdapter(new ArrayAdapter<String>(getActivity(), itemLayoutId, list));
+        Log.i(TAG, "addDevice: " + deviceName);
+         adapter.add(deviceName);
     }
 
 
+    public class RVAdapter extends RecyclerView.Adapter<BluetoothFragment.RVAdapter.BluetoothViewHolder> {
+
+
+        List<String> devices;
+
+        public RVAdapter(List<String> devices) {
+            this.devices = devices;
+        }
+
+        @Override
+        public BluetoothFragment.RVAdapter.BluetoothViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_bluetooth, parent, false);
+            return new BluetoothFragment.RVAdapter.BluetoothViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(BluetoothFragment.RVAdapter.BluetoothViewHolder holder, int position) {
+            String device = devices.get(position);
+            holder.device.setText(device);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return devices.size();
+        }
+
+        public void setData(List<String> data) {
+            this.devices = data;
+        }
+
+        public void add(String device) {
+            this.devices.add(device);
+            notifyDataSetChanged();
+        }
+
+        public class BluetoothViewHolder extends RecyclerView.ViewHolder {
+
+            TextView device;
+
+            BluetoothViewHolder(View itemView) {
+                super(itemView);
+                device = (TextView) itemView.findViewById(R.id.device);
+            }
+        }
+
+
+    }
 }
+
