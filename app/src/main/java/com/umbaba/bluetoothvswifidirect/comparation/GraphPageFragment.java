1f4827a4774e43d60a70bfb5a8392434d2446ab6
Chickinnick <chickinnick@gmail.com>
2017-05-27 23:01:52 +0300

    graph lib

--- app/src/main/java/com/umbaba/bluetoothvswifidirect/comparation/GraphPageFragment.java
+++ app/src/main/java/com/umbaba/bluetoothvswifidirect/comparation/GraphPageFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.umbaba.bluetoothvswifidirect.R;
 import com.umbaba.bluetoothvswifidirect.data.comparation.Criteria;
 
@@ -44,7 +47,15 @@ public class GraphPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_graph_page, container, false);
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+        return view;
     }
 
 }
